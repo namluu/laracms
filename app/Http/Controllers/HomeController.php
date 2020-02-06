de0cba11dc9591547d17457f646537be5fa250a8
Nam Luu <nam.luuduc@gmail.com>
2020-02-07 00:17:32 +0700

    Frontend - load category menu

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Category;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $categories = Category::where('is_active', 1)
+            ->orderBy('name', 'desc')
+            ->get();
         $posts = Post::get();
-        return view('home', compact('posts'));
+        return view('home', compact(['categories', 'posts']));
     }
 }
